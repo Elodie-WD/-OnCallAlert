1d276fa90451d8ebe6666d1262daa9d63dec5a9a
elodie ettaiche <e.ettaiche@gmail.com>
2023-12-05 14:51:09 +0100

    scss chatroom

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,11 +2,11 @@ class ContactsController < ApplicationController
   def show
     @booking = Booking.where(date: Date.today, user: current_user).first
     if @booking.on_call
-      @title = "Personne de Garde"
+      @title = "Personne de garde"
       @on_duty = Booking.where(date: Date.today, on_call: false).first.user_id
       @contact = User.all.find { |user| user.id == @on_duty }
     elsif !@booking.on_call
-      @title = "Personne d'Astreinte"
+      @title = "Personne d'astreinte"
       @on_call = Booking.where(date: Date.today, on_call: true).first.user_id
       @contact = User.all.find { |user| user.id == @on_call }
     else
