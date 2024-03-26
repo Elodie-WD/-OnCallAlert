31e0bea27ae1919fa29725057f4381957be4f079
elodie ettaiche <e.ettaiche@gmail.com>
2024-03-26 16:45:30 +0100

    add flatpicker/create new booking/modif of booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
+  validates :date, presence: true
+  validates :date, uniqueness: { scope: :on_call, if: :on_call?,
+    message: " non valide : Astreinte déjà attribuée"  }
+  validates :date, uniqueness: { scope: :on_call, unless: :on_call?,
+    message: " non valide : Garde déjà attribuée"  }
 end
