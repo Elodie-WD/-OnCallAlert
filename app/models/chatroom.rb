f27de0b8c21b86f4cd1e36d0046c03322af93752
elodie ettaiche <e.ettaiche@gmail.com>
2024-04-12 17:15:49 +0200

    mod chatroom model/controller/view - messages history dynamic/spec

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,4 +3,12 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   validates :date, presence: true
   has_many :users, through: :messages
+
+
+def chatroom_contact(date, user)
+  Booking.all.where(date: date).where.not(user: user).first.user.first_name
+end
+
+
+
 end
