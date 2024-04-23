eccd58089ee56b41d75eb38e602daa1a04315da8
elodie ettaiche <e.ettaiche@gmail.com>
2024-04-23 16:32:57 +0200

    refacto controller user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,31 @@
 class UsersController < ApplicationController
+  before_action :on_call_booking, :set_user, only: %i[on_call on_duty default]
+  # before_action :set_user, only: %i[on_call on_duty default]
   def on_call
     planning_date
-    @booking_contact = Booking.find_by(date: Date.today, on_call: true)
-    @contact = @booking_contact.user
+    # @booking_contact = Booking.find_by(date: Date.today, on_call: true)
+    # @contact = @booking_contact.user
   end
 
   def on_duty
     planning_date
-    (@booking_contact = Booking.find_by(date: Date.today, on_call: true))?(@contact = @booking_contact.user):(@contact = "Attention pas d'attribution")
+    (@on_call_booking)?(@on_call_contact):(@on_call_contact = "Attention pas d'attribution")
   end
 
   def default
     planning_date
-    @booking_contact_on_call = Booking.find_by(date: Date.today, on_call: true).user
-    @booking_contact_on_duty = Booking.find_by(date: Date.today, on_call: false).user
+    @on_duty_contact = Booking.find_by(date: Date.today, on_call: false).user
   end
 
   def update
     # @on_call = Booking.find_by(date: Date.today, on_call: true)
     # @contact = @on_call.user
-    @contact = User.find(params[:id])
-    @contact.update(deplacement: params[:deplacement].to_i)
-    redirect_to contact_path if @contact.called?
-    redirect_to map_path if @contact.accepted?
-    redirect_to on_call_path if @contact.rejected?
-    redirect_to on_call_path if @contact.uncalled?
+    @on_call_contact = User.find(params[:id])
+    @on_call_contact.update(deplacement: params[:deplacement].to_i)
+    redirect_to contact_path if @on_call_contact.called?
+    redirect_to map_path if @on_call_contact.accepted?
+    redirect_to on_call_path if @on_call_contact.rejected?
+    redirect_to on_call_path if @on_call_contact.uncalled?
     # enum :deplacement, { uncalled: 0, called: 1, accepted: 2, rejected: 3 }
   end
 
@@ -47,6 +48,37 @@ class UsersController < ApplicationController
 
   private
 
+  def on_call_booking
+    @on_call_booking = Booking.find_by(date: Date.today, on_call: true)
+    @on_call_contact = @on_call_booking.user
+    # @contact = @booking_contact.user
+
+
+  end
+
+  def set_user
+    @booking_user = Booking.find_by(date: Date.today, user: current_user)
+
+
+  end
+
+
+
+  def planning_date
+    @user = current_user
+    if Booking.find_by(user: current_user) == nil
+      # raise
+      @on_call = nil
+      @on_duty = nil
+    else
+      # raise
+    # @booking_user = Booking.find_by(date: Date.today, user: current_user)
+    @on_call = current_user.next_booking(true)
+    @on_duty = current_user.next_booking(false)
+    end
+  end
+end
+
   # def planning_date
   #  @user = current_user
   #  @booking = Booking.find_by(date: Date.today, user: current_user)
@@ -64,18 +96,3 @@ class UsersController < ApplicationController
   #    @on_duty = @on_duty.first
   #  end
   # end
-
-  def planning_date
-    @user = current_user
-    if Booking.find_by(user: current_user) == nil
-      # raise
-      @on_call = nil
-      @on_duty = nil
-    else
-      # raise
-    @booking = Booking.find_by(date: Date.today, user: current_user)
-    @on_call = current_user.next_booking(true)
-    @on_duty = current_user.next_booking(false)
-    end
-  end
-end
